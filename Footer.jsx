d23eb9a881f9e3eb1d5cef6e28c3394691fb1403
lekshmi-02 <85623989+lekshmi-02@users.noreply.github.com>
2021-06-26 23:41:41 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+  return(
+    <footer>
+      <p>Copyright By ShapeAI@{new Date().getFullYear()}</p>
+    </footer>
+    );
+}
+export default Footer;
